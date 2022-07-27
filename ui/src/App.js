ed866a27152d53718596f6f8397afc5844ed3149
Tom Radford <tom@theradford.com>
2022-07-27 16:17:51 +0200

    Apollo client examples

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,33 +1,38 @@
-import { gql, useQuery } from '@apollo/client'
-
-const ALL_PERSONS = gql`
-  query {
-    allPersons {
-      name
-      phone
-      id
-    }
-  }
-`
+import { useQuery } from '@apollo/client'
+import { useState } from 'react'
+import Persons from './Person'
+import PersonForm from './PersonForm'
+import PhoneForm from './PhoneForm'
+import { ALL_PERSONS } from './queries'
 
 const App = () => {
+  // const result = useQuery(ALL_PERSONS, { pollInterval: 1000 })
+  const [errorMessage, setErrorMessage] = useState(null)
   const result = useQuery(ALL_PERSONS)
   if (result.loading) {
     return <div>Loading....</div>
   }
 
-  return <Persons persons={result.data.allPersons} />
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
+  return (
+    <div>
+      <Notify errorMessage={errorMessage} />
+      <Persons persons={result.data.allPersons} />
+      <PersonForm setError={notify} />
+      <PhoneForm setError={notify} />
+    </div>
+  )
 }
 
-const Persons = ({ persons }) => (
-  <div>
-    <h2>Persons</h2>
-    {persons.map((p) => (
-      <div key={p.id}>
-        {p.name} {p.phone}
-      </div>
-    ))}
-  </div>
-)
+const Notify = ({ errorMessage }) => {
+  if (!errorMessage) return null
+  return <div style={{ color: 'red' }}>{errorMessage}</div>
+}
 
 export default App
